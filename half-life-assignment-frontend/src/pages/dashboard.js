c07784187a4eeeaecbb49000d9730b8dbf5b2dd2
Nipun Lakshan <apnipun@gmail.com>
2023-09-17 14:48:48 +0530

    change track button

--- half-life-assignment-frontend/src/pages/dashboard.js
+++ half-life-assignment-frontend/src/pages/dashboard.js
@@ -19,9 +19,7 @@ import Header from "../components/header";
 
 function Dashboard (){
     const [createModal, setCreateModal] = useState(false);
-    const [trackModal, setTrackModal] = useState(false);
     const createBtnClick = () => setCreateModal(!createModal);
-    const trackBtnClick = () => setTrackModal(!trackModal);
     return (
         <>
             <Header/>
@@ -31,9 +29,6 @@ function Dashboard (){
             }}>
                 <MDBBtn style={{marginRight: '20px'}} type='button' onClick={createBtnClick}>
                     Create Shipment
-                </MDBBtn>
-                <MDBBtn type='button' onClick={trackBtnClick}>
-                    Track Shipment
                 </MDBBtn>
             </div>
             <div className='m-5' style={{
@@ -81,67 +76,8 @@ function Dashboard (){
                                 <MDBBtn color='link' rounded size='sm'>
                                     Edit
                                 </MDBBtn>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div className='d-flex align-items-center'>
-                                    <div>
-                                        <p className='fw-bold mb-1'>Alex Ray</p>
-                                        <p className='text-muted mb-0'>alex.ray@gmail.com</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className='d-flex align-items-center'>
-                                    <div>
-                                        <p className='fw-bold mb-1'>Alex Ray</p>
-                                        <p className='text-muted mb-0'>alex.ray@gmail.com</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <p className='fw-normal mb-1'>Consultant</p>
-                            </td>
-                            <td>
-                                <MDBBadge color='primary' pill>
-                                    Onboarding
-                                </MDBBadge>
-                            </td>
-                            <td>
                                 <MDBBtn color='link' rounded size='sm'>
-                                    Edit
-                                </MDBBtn>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div className='d-flex align-items-center'>
-                                    <img
-                                        src='https://mdbootstrap.com/img/new/avatars/7.jpg'
-                                        alt=''
-                                        style={{ width: '45px', height: '45px' }}
-                                        className='rounded-circle'
-                                    />
-                                    <div className='ms-3'>
-                                        <p className='fw-bold mb-1'>Kate Hunington</p>
-                                        <p className='text-muted mb-0'>kate.hunington@gmail.com</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <p className='fw-normal mb-1'>Designer</p>
-                                <p className='text-muted mb-0'>UI/UX</p>
-                            </td>
-                            <td>
-                                <MDBBadge color='warning' pill>
-                                    Awaiting
-                                </MDBBadge>
-                            </td>
-                            <td>Senior</td>
-                            <td>
-                                <MDBBtn color='link' rounded size='sm'>
-                                    Edit
+                                    Track Shipment
                                 </MDBBtn>
                             </td>
                         </tr>
@@ -168,33 +104,6 @@ function Dashboard (){
                         </MDBModalBody>
                         <MDBModalFooter>
                             <MDBBtn color='secondary' onClick={createBtnClick}>
-                                Close
-                            </MDBBtn>
-                            <MDBBtn>Save changes</MDBBtn>
-                        </MDBModalFooter>
-                    </MDBModalContent>
-                </MDBModalDialog>
-            </MDBModal>
-
-            {/*Track Shipment Modal*/}
-            <MDBModal show={trackModal} setShow={setTrackModal} tabIndex='-1'>
-                <MDBModalDialog>
-                    <MDBModalContent>
-                        <MDBModalHeader>
-                            <MDBModalTitle>Track Shipment</MDBModalTitle>
-                            <MDBBtn className='btn-close' color='none' onClick={trackBtnClick}></MDBBtn>
-                        </MDBModalHeader>
-                        <MDBModalBody>
-                            <form>
-                                <MDBInput className='mb-4 w-100' type='name' id='name' label='Full Name' />
-                                <MDBInput className='mb-4 w-100' type='email' id='email' label='Email Address' />
-                                <MDBTextArea className='mb-4 w-100' type='address' id='address' label='Address' />
-                                <MDBInput className='mb-4 w-100' type='password' id='password' label='Password' />
-                                <MDBInput className='mb-4 w-100' type='confirmPassword' id='confirmPassword' label='Confirm Password' />
-                            </form>
-                        </MDBModalBody>
-                        <MDBModalFooter>
-                            <MDBBtn color='secondary' onClick={trackBtnClick}>
                                 Close
                             </MDBBtn>
                             <MDBBtn>Save changes</MDBBtn>
