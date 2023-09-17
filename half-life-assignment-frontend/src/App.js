ddd947c0623b9114dff32f7bba7ae573597f63e0
Nipun Lakshan <apnipun@gmail.com>
2023-09-17 08:25:33 +0530

    added mdb bootstrap

--- half-life-assignment-frontend/src/App.js
+++ half-life-assignment-frontend/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div className="form-outline mb-4">
+          <input type="email" id="form2Example1" className="form-control"/>
+          <label className="form-label" htmlFor="form2Example1">Email address</label>
+        </div>
+
+        <div className="form-outline mb-4">
+          <input type="password" id="form2Example2" className="form-control"/>
+          <label className="form-label" htmlFor="form2Example2">Password</label>
+        </div>
+
+        <div className="row mb-4">
+          <div className="col d-flex justify-content-center">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="form2Example31" checked/>
+              <label className="form-check-label" htmlFor="form2Example31"> Remember me </label>
+            </div>
+          </div>
+
+          <div className="col">
+            <a href="#!">Forgot password?</a>
+          </div>
+        </div>
+
+        <button type="button" className="btn btn-primary btn-block mb-4">Sign in</button>
+
+        <div className="text-center">
+          <p>Not a member? <a href="#!">Register</a></p>
+          <p>or sign up with:</p>
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-facebook-f"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-google"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-twitter"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-github"></i>
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
